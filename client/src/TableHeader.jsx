8266da4159c664b8fa68843c6ea5738393895944
martamoroni <marta.moroni0203@stud.hkr.se>
2025-05-26 01:48:16 +0200

    Cleaned comment

--- client/src/TableHeader.jsx
+++ client/src/TableHeader.jsx
@@ -1,20 +1,9 @@
 import { IoIosArrowDown } from "react-icons/io";
 
 function TableHeader(props) {
-  // Function to sort in ascending order based on a key (column selected)
-  // const sortColumn = (key) => {
-  //   const sortedAssignments = [...props.assignments].sort((a, b) => {
-  //     if (a[key] < b[key]) return -1;
-  //     if (a[key] > b[key]) return 1;
-  //     return 0;
-  //   });
-
-  //   props.setAssignments(sortedAssignments); // update state to show new table
-  //   props.setSortingKey(key); // update sorting state
-  // };
-
   return (
     <th
+      // to sort in ascending order based on a key (column selected)
       onClick={() => {
         const sortedAssignments = [...props.assignments].sort((a, b) => {
           if (a[props.sortingKeyValue] < b[props.sortingKeyValue]) return -1;
