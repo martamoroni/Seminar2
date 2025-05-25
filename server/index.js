4bc925c9cb2e1e0ddd374960a5b537a369413dea
martamoroni <marta.moroni0203@stud.hkr.se>
2025-05-26 01:11:17 +0200

    Serve from dist directory

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
 const apiRoutes = require("./src/routes/index");
-const cors = require("cors"); // to make API calls from frontend
+const cors = require("cors");
 const connectDB = require("./src/config/db");
 
 dotenv.config();
@@ -11,6 +11,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+// To make API calls from frontend
 app.use(
   cors({
     origin: "http://localhost:5173", // Only allow this origin
@@ -19,6 +20,14 @@ app.use(
 
 // Mount API routes
 app.use("/api", apiRoutes);
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// Handle all other routes by serving the index.html
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
